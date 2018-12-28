d5249a7023c06c37b6108513106f1a35faa16742
Furkan <furkaaan28@live.de>
2018-12-28 19:19:08 +0100

    RRify

--- rr_klte.mk
+++ rr_klte.mk
@@ -15,10 +15,10 @@
 # limitations under the License.
 #
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common RR stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/klte/full_klte.mk)
 
 PRODUCT_DEVICE := klte
-PRODUCT_NAME := lineage_klte
+PRODUCT_NAME := rr_klte
